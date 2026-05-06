0fac37516bedbd4203705defeddce76ca678b3db
Gregorius-Yves <gregoriusyves@gmail.com>
2026-05-06 14:52:10 +0700

    chore: cleanup, since we're not using oracle anymore

--- cobol/src/register_bridge.c
+++
@@ -1,8 +0,0 @@
-#include <libcob.h>
-
-extern int REGISTER(char *name, char *pin, char *result);
-
-void register_bridge(char *name, char *pin, char *result) {
-    cob_init(0, NULL);
-    REGISTER(name, pin, result);
-}
